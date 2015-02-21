9e4efc7647c350bdcfe8507c09f89857a3b3a5bc
Stephen Papierski <stephenpapierski@gmail.com>
2015-02-21 17:24:36 -0500

    simple shell: execute external program given full path

--- shell.cpp
+++ shell.cpp
@@ -1,7 +1,62 @@
+/**
+ * @file shell.cpp
+ * @author Stephen Papierski <stephenpapierski@gmail.com>
+ * @version 0.1
+ */
+#include <stdio.h>
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include "shell.h"
 
 using namespace std;
 
 int main(){
-    cout<<"hello world\n";
+    char command[1024];
+    char *argv[64];
+    
+    //while (1)
+    while(1){
+        cout<<"$ ";
+        cin.getline(command,1024); //read in command line
+        split(command,argv); //generates argument vector
+
+        execute_program(argv); //execute program
+    }
+    return 0;
+}
+
+/**
+ * Processes command line argument and generates the argument vector
+ */
+void split(char *line, char **argv){
+    while (*line != '\0'){ //if not EOL
+        while (*line == ' ') //replace spaces with \0
+            *line++ = '\0'; 
+        *argv++ = line; //store address of argument
+        while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n')
+            line++;
+    }
+    *argv = '\0'; //end of argument vector
+}
+
+/**
+ * Executes a program
+ */
+void execute_program(char **argv){
+    int status;
+    pid_t pid = fork();
+
+    if (pid == -1){
+        //fork error
+        cout<<"Fork error...";
+    } else if (pid == 0){
+        //child process
+        execv(*argv, argv);
+    } else if (pid > 0){
+        waitpid(-1, &status, WUNTRACED);
+        //shell process
+    }
 }
---
+++ shell.h
@@ -0,0 +1,3 @@
+int main(void);
+void split(char *, char **);
+void execute_program(char **);
