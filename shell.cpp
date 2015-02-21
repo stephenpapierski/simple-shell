bb23e7215971efc451be9b35589f16fe63a3310b
Stephen Papierski <stephenpapierski@gmail.com>
2015-02-21 18:35:39 -0500

    Added support for exit, cd, pwd, and display version information

--- shell.cpp
+++ shell.cpp
@@ -2,12 +2,16 @@
  * @file shell.cpp
  * @author Stephen Papierski <stephenpapierski@gmail.com>
  * @version 0.1
+ * @date 22 February 2015
  */
+#include <stdlib.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <iostream>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <cstring>
 
 #include "shell.h"
 
@@ -16,14 +20,33 @@ using namespace std;
 int main(){
     char command[1024];
     char *argv[64];
+    char cwd[1024];
+    getcwd(cwd,sizeof(cwd));
     
     //while (1)
     while(1){
+        cout<<"SS_";               //can be removed...tells you you're in simple shell
+        printf("%s$ ", cwd);
         cout<<"$ ";
         cin.getline(command,1024); //read in command line
         split(command,argv); //generates argument vector
 
-        execute_program(argv); //execute program
+        if (argv[0] == NULL){
+            //do nothing
+        }else if (strcmp(argv[0],"exit") == 0){         //exit
+            exit(EXIT_SUCCESS);
+        }else if (strcmp(argv[0],"cd") == 0){           //cd
+            if (chdir(argv[1]) != 0)
+                cout<<"No such file or directory\n";
+        }else if (strcmp(argv[0], "pwd") == 0){         //pwd
+            if (getcwd(cwd,sizeof(cwd)) != NULL)
+                printf("%s\n", cwd);
+        }else if (strcmp(argv[0], "version") == 0){     //version
+            cout<<version;
+        }else{
+            execute_program(argv);                      //execute program
+        }
+
     }
     return 0;
 }
@@ -51,12 +74,14 @@ void execute_program(char **argv){
 
     if (pid == -1){
         //fork error
-        cout<<"Fork error...";
+        cout<<"Fork error...\n";
     } else if (pid == 0){
         //child process
-        execv(*argv, argv);
+        if (execv(*argv, argv) == -1)
+            printf("%s: command not found\n", argv[0]); 
     } else if (pid > 0){
         waitpid(-1, &status, WUNTRACED);
         //shell process
     }
+    return;
 }
--- shell.h
+++ shell.h
@@ -1,3 +1,5 @@
+char version[] = "Simple Shell 0.1\nCopyright 2015 Stephen Papierski\nThis is free software.\nThere is NO WARRANTY, to the extent permitted by law.\nWritten by Stephen Papierski <stephenpapierski@gmail.com>\n";
+
 int main(void);
 void split(char *, char **);
 void execute_program(char **);
