d2a404f7af6f8959d46b9db62a4508dfd674cb2b
Stephen Papierski <stephenpapierski@gmail.com>
2015-02-22 20:03:28 -0500

    Backgrounding support added

--- shell.cpp
+++ shell.cpp
@@ -12,6 +12,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <cstring>
+#include <list>
 
 #include "shell.h"
 
@@ -20,7 +21,6 @@ using namespace std;
 int main(){
     char command[1024];
     char *argv[64];
-    char cwd[1024];
     //getcwd(cwd,sizeof(cwd));
     
     while(1){
@@ -29,26 +29,81 @@ int main(){
         //cout<<"$ ";
         cin.getline(command,1024); //read in command line
         split(command,argv,' '); //generates argument vector
-
-        if (argv[0] == NULL){
-            //do nothing
-        }else if (strcmp(argv[0],"exit") == 0){         //exit
-            exit(EXIT_SUCCESS);
-        }else if (strcmp(argv[0],"cd") == 0){           //cd
-            if (chdir(argv[1]) != 0)
-                cout<<"No such file or directory\n";
-        }else if (strcmp(argv[0], "pwd") == 0){         //pwd
-            if (getcwd(cwd,sizeof(cwd)) != NULL)
-                printf("%s\n", cwd);
-        }else if (strcmp(argv[0], "version") == 0){     //version
-            cout<<version;
-        }else{
-            execute_program(argv);                      //execute program
-        }
-
+        process_argument(argv);
     }
     return 0;
 }
+
+/**
+ * This function handles build in shell functions as well as doing the setup for executing programs
+ * @brief Decides how to handle arguments
+ * @param[in] argv The argument vector
+ */
+void process_argument(char **argv){
+    static char cwd[1024];
+    if (argv[0] == NULL){
+        //do nothing
+    }else if (strcmp(argv[0],"exit") == 0){         //exit
+        exit(EXIT_SUCCESS);
+    }else if (strcmp(argv[0],"cd") == 0){           //cd
+        if (chdir(argv[1]) != 0)
+            cout<<"No such file or directory\n";
+    }else if (strcmp(argv[0], "pwd") == 0){         //pwd
+        if (getcwd(cwd,sizeof(cwd)) != NULL)
+            printf("%s\n", cwd);
+    }else if (strcmp(argv[0], "version") == 0){     //version
+        cout<<version;
+    }else{
+        bool background = false;
+        if (strcmp(argv[arraySize(argv)-1], "&") == 0){ //checks for & background option
+            argv[arraySize(argv)-1] = NULL; //removes & from argument vector
+            background = true;
+        }
+        execute_program(argv, background); //execute program
+    }
+}
+
+/**
+ * Executes a program
+ * @param[in]   argv        The argument vector
+ * @param[in]   background  Should the program be backgrounded?
+ */
+void execute_program(char **argv, bool background){
+    int status;
+    static list<pid_t> children;
+    static int programs_backgrounded = 0;
+    static int backgrounds_finished = 0;
+    pid_t pid = fork(); //returns 0 to the child, childs pid to the parent
+
+    if (pid == -1){
+        //fork error
+        cout<<"Fork error...\n";
+    }else if (pid == 0){
+        //child process
+        if (execvp(argv[0], argv) == -1)
+            printf("%s: command not found\n", argv[0]); 
+    }else if (pid > 0){
+        //shell process
+        for (list<pid_t>::iterator i = children.begin(); i != children.end(); i++){ //iterate through traced pid's
+            pid_t killed = waitpid(*i, &status, WNOHANG|WUNTRACED); //reaps any children that are finished
+            if (killed >0){
+                children.erase(i++); //removes killed children from background list
+                backgrounds_finished++;
+                printf("[%d] Done\n", backgrounds_finished);
+            }
+        }
+        if (background){
+            children.push_back(pid); //add background process to traced children
+            programs_backgrounded++;
+            printf("[%d] %d\n", programs_backgrounded, pid);
+        }
+        waitpid(pid, &status, (background) ? WNOHANG : WUNTRACED);
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------------
+//UTILITY FUNCTIONS
+//-----------------------------------------------------------------------------------------------------------
 
 /**
  * Splits a given string into an array based on the delimeter
@@ -69,21 +124,14 @@ void split(char *line, char **split, char delim){
 }
 
 /**
- * Executes a program
+ * Finds the number of non-null items in an array
+ * @param[in]   array   The array to be sized up
+ * @return      number of non-null items in array
  */
-void execute_program(char **argv){
-    int status;
-    pid_t pid = fork();
-
-    if (pid == -1){
-        //fork error
-        cout<<"Fork error...\n";
-    }else if (pid == 0){
-        //child process
-        if (execvp(argv[0], argv) == -1)
-            printf("%s: command not found\n", argv[0]); 
-    }else if (pid > 0){
-        //shell process
-        waitpid(-1, &status, WUNTRACED);
+int arraySize(char **array){
+    int i = 0;
+    while (array[i] != NULL){
+        i++;
     }
+    return i;
 }
--- shell.h
+++ shell.h
@@ -1,5 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+//-------------------------------------------------------------------
+//VARIABLES
+//-------------------------------------------------------------------
 char version[] = "Simple Shell 0.1\nCopyright 2015 Stephen Papierski\nThis is free software.\nThere is NO WARRANTY, to the extent permitted by law.\nWritten by Stephen Papierski <stephenpapierski@gmail.com>\n";
 
+//------------------------------------------------------------------
+//FUNCTION PROTOTYPES
+//------------------------------------------------------------------
 int main(void);
+void process_argument(char **);
+void execute_program(char **, bool);
 void split(char *, char **, char);
-void execute_program(char **);
+int arraySize(char **);
+
+#endif
